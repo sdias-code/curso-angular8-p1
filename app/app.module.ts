a50544eccc5c77a40c16510d08de322f1eb25df1
sdias-code <silviodias.ms@gmail.com>
2020-11-22 02:12:36 -0400

    Ativando Rotas para acessar componentes

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,6 +9,8 @@ import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
+
 
 @NgModule({
   declarations: [
@@ -17,21 +19,27 @@ import { Error404Component } from './error-404/error-404.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', 
+        component: CourseInfoComponent
+      },
+      {
         path: '', 
         redirectTo: 'courses', 
         pathMatch: 'full'
       },
-      {
-        path: 'courses',
-        component: CourseListComponent
-      },
+      
       {
         path: '**',
         component: Error404Component
